3846d498bf6206c29cd33c94af34668fe6f16806
Onur-TURAN <onurturan.t@gmail.com>
2023-01-07 15:16:01 +0300

    alev alev:fire:

--- HackerRank/C/1_hello-world/main.c
+++ HackerRank/C/1_hello-world/main.c
@@ -7,7 +7,7 @@ int main()
 {
 	char s[100];
 	scanf("%[^\n]%*c", &s);
-  	printf("Hello, World!\n%s", s);
+  	printf("Hello, World! \n%s", s);
 		
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */    
 	
