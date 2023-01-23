f1595f1bde989711300920d39f1a3c13b0677f7d
Onur-TURAN <onurturan.t@gmail.com>
2023-01-23 20:42:23 +0300

    :fire::fire::fire:

---
+++ HackerRank/C/7_sum-of-digits-five/main2.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+  int n, sum = 0;
+  scanf("%d", &n);
+  while (n != 0) {
+    sum += n % 10;
+    n = n / 10;
+  }
+  printf("%d", sum);
+  return 0;
+}
+
