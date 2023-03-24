0da6d843857521394179540caef0d4fc86a6fdc9
Onur-TURAN <onurturan.t@gmail.com>
2023-03-24 17:46:00 +0300

    :fire:fire:fire:

--- HackerRank/C/11_printing-pattern-using-loops/main.c
+++ HackerRank/C/11_printing-pattern-using-loops/main.c
@@ -1,34 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
 int main() 
 {
 
-    int n,a;
+    int n;
     scanf("%d", &n);
-    a = n;
-  	int i, j;
-	for(i=1;i<=2*n-1;i++)
-	{
-		if(j<n-abs(n-i))
-		{
-			printf("%d ",a);
-			a--;
-		}
-		else if(j>n+abs(n-i))
-		{
-			a++;
-			printf("%d ",a);
-		}
-		else
-		{
-			printf("%d ",a);
-		}
-	}
-	printf("\n");
-
-
+    int arr[2*n-1][2*n-1];
+    
+    // initialize all the array with n
+    // then replace everything except for the outer ring with n - 1 ...
+    // until reaching 1
+    for (int j = 0; j < n; j++){
+        for (int i = j; i <= 2*n - 2 - j; i++){
+            for (int k = j; k <= 2*n - 2 - j; k++){
+                arr[i][k] = n - j;
+            }
+        }
+    }
+    
+    // print the array    
+    for (int i = 0; i < 2*n - 1; i++){
+        for (int j = 0; j < 2*n -1; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
