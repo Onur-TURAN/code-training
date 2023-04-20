12692754288e409fa3a956ca5504ed99245098b5
Onur-TURAN <onurturan.t@gmail.com>
2023-04-21 00:09:04 +0300

    :fire:fire:fire:

--- gobyexample/2_values/value.go
+++ gobyexample/2_values/value.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("go" + "lang")
 	fmt.Println("1+1=", 1+1)
 	fmt.Println("7.0/3.0=", 7.0/3.0)
-	fmt.Println(true && false) || ve anlamına gelmektedir.
+	fmt.Println(true && false) // || ve anlamına gelmektedir.
 	fmt.Println(true ||false) // || ya da anlamına gelmektedir.
 	fmt.Println(!true) //! isareti degili demektir.
